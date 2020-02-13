c0a4e060a7a44d869268dcc56e321bc3e8372a70
Cesare Rossi <cesare.rossi@terradue.com>
2020-02-13 12:36:58 +0100

    Added decode and encode tests

---
+++ cmd/wpsgrabber/decode_test.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+)
+
+func TestParseExecuteResponse(t *testing.T) {
+
+	currentWorkingDir, _ := os.Getwd()
+
+	exampleSucceeded := filepath.Join(currentWorkingDir, "resources", "example_succeeded.xml")
+	exampleFailed := filepath.Join(currentWorkingDir, "resources", "example_failed.xml")
+
+	var tests = []struct {
+		path         string
+		status       int
+		creationTime string
+		identifier   string
+		version      string
+		title        string
+	}{
+		{exampleSucceeded,
+			0,
+			"2016-04-26T09:08:06Z",
+			"com.terradue.wps_oozie.process.OozieAbstractAlgorithm",
+			"1.0.0",
+			"SRTM Digital Elevation Model",
+		},
+		{exampleFailed,
+			1,
+			"2016-04-26T09:16:58Z",
+			"com.terradue.wps_oozie.process.OozieAbstractAlgorithm",
+			"1.0.0",
+			"SRTM Digital Elevation Model",
+		},
+	}
+
+	loc, _ := time.LoadLocation("UTC")
+
+	for _, test := range tests {
+		response, err := parseExecuteResponse(test.path)
+
+		if err != nil {
+			t.Errorf("parseExecuteResponse failed using %s", test.path)
+		}
+
+		if response.Status.ProcessStatus != test.status {
+			t.Errorf("parseExecuteResponse returned status %d instead of %d", response.Status.ProcessStatus, test.status)
+		}
+
+		parsedCreationTime := response.Status.CreationTime.In(loc).Format(time.RFC3339)
+
+		if parsedCreationTime != test.creationTime {
+			t.Errorf("parseExecuteResponse returned creationTime %s instead of %s", parsedCreationTime, test.creationTime)
+		}
+
+		if response.Process.Identifier != test.identifier {
+			t.Errorf("parseExecuteResponse returned identifier %s instead of %s", response.Process.Identifier, test.identifier)
+		}
+
+		if response.Process.Version != test.version {
+			t.Errorf("parseExecuteResponse returned creationTime %s instead of %s", response.Process.Version, test.version)
+		}
+
+		if response.Process.Title != test.title {
+			t.Errorf("parseExecuteResponse returned creationTime %s instead of %s", response.Process.Title, test.title)
+		}
+	}
+}
---
+++ cmd/wpsgrabber/encode_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"github.com/google/uuid"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+)
+
+func TestCreateCSV(t *testing.T) {
+
+	var status Status
+	var process Process
+	var response ExecuteResponse
+
+	status.ProcessStatus = 0
+	status.CreationTime, _ = time.Parse(time.RFC3339, "2020-04-26T11:16:58.912+02:00")
+	status.EndTime, _ = time.Parse(time.RFC3339, "2020-04-30T11:16:58.912+02:00")
+	status.ProcessStatus = 0
+
+	process.Identifier = "com.terradue.wps_oozie.process.OozieAbstractAlgorithm"
+	process.Version = "1.0.0"
+	process.Title = "SRTM Digital Elevation Model"
+	process.WorkflowIdentifier = ""
+
+	response.Process = process
+	response.Status = status
+
+	CSVfilename := filepath.Join(configuration.CSVOutputDir, uuid.New().String()+".csv")
+
+	err := createCSV(CSVfilename, &response)
+
+	if err != nil {
+		t.Errorf("createCSV failed")
+	}
+
+	CSVfile, err := os.Open(CSVfilename)
+
+	if err != nil {
+		t.Errorf("can't open file: %s", CSVfilename)
+	}
+
+	defer CSVfile.Close()
+
+	var expectedCSV = "com.terradue.wps_oozie.process.OozieAbstractAlgorithm,1.0.0,,not used,2020-04-26T09:16:58Z,2020-04-30T09:16:58Z,0"
+
+	scanner := bufio.NewScanner(CSVfile)
+	scanner.Scan()
+	actualCSV := scanner.Text()
+
+	if actualCSV != expectedCSV {
+		t.Errorf("createCSV returned %s instead of %s", actualCSV, expectedCSV)
+	}
+
+	os.Remove(CSVfilename)
+}
